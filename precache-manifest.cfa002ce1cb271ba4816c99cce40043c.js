0dc8be0f4c744f6e1d5493a7db51cc968f41893f
Noel De Martin <noeldemartin@gmail.com>
2018-08-18 12:59:32 +0200

    deploy

---
+++ precache-manifest.cfa002ce1cb271ba4816c99cce40043c.js
@@ -0,0 +1,71 @@
+self.__precacheManifest = [
+  {
+    "url": "/planning-poker/c0c13389e0c54cae7ffc4e8189a7c6ca.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/planning-poker/robots.txt"
+  },
+  {
+    "url": "/planning-poker/ff0919ae6b095a75bd0dfbbfbedebcc7.png"
+  },
+  {
+    "revision": "221d87d4e6af17ade938",
+    "url": "/planning-poker/css/chunk-vendors.e9f903bf.css"
+  },
+  {
+    "url": "/planning-poker/fa1d10e7f87f2cda63c552426b75375a.png"
+  },
+  {
+    "revision": "8cdadd9c06c0ea1a87abed3713764b14",
+    "url": "/planning-poker/index.html"
+  },
+  {
+    "url": "/planning-poker/fd9db3736cfc3a2bd85fed420c3f8894.png"
+  },
+  {
+    "url": "/planning-poker/e9ab56291cbac7436ec0ad0e02d1df4b.png"
+  },
+  {
+    "url": "/planning-poker/ff325f82397728e998340d64fd775562.png"
+  },
+  {
+    "revision": "221d87d4e6af17ade938",
+    "url": "/planning-poker/js/chunk-vendors.64de24d0.js"
+  },
+  {
+    "revision": "cf0e47652ad24bafdf70",
+    "url": "/planning-poker/js/app.75f14c5c.js"
+  },
+  {
+    "revision": "cf0e47652ad24bafdf70",
+    "url": "/planning-poker/css/app.d6e7e569.css"
+  },
+  {
+    "url": "/planning-poker/ce68cf66981ad5ab49e786026cb1fc62.png"
+  },
+  {
+    "url": "/planning-poker/c67fcd80821e29999960ce0b7338fb9a.png"
+  },
+  {
+    "url": "/planning-poker/b9d3632c892372f7031d38d66feeece0.png"
+  },
+  {
+    "url": "/planning-poker/a04b591d4d96c5e7480ae3a9b16d0e88.png"
+  },
+  {
+    "url": "/planning-poker/9cd9de53e26d23b6fcb7d4b972f95ef7.png"
+  },
+  {
+    "url": "/planning-poker/98d6a8aeec2fbcba94bac0dc7c2a8f7e.png"
+  },
+  {
+    "url": "/planning-poker/93edc0245e727ed784d1997c97d77f09.png"
+  },
+  {
+    "url": "/planning-poker/457cfbe61892da22757b7865968b8e7c.png"
+  },
+  {
+    "url": "/planning-poker/0b39bb509ebc8dab660694f09e2328d3.png"
+  }
+];
